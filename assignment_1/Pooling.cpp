cae9fd9972876f652b8e7f5914988acfa79f0079
Harshcs517 <cs5170409@cse.iitd.ac.in>
2019-01-12 17:52:16 +0530

    small changes in convolution.cpp

--- assignment_1/Pooling.cpp
+++ assignment_1/Pooling.cpp
@@ -65,6 +65,6 @@ Vector_Matrix Pooling(Vector_Matrix v1, string pool,int stride)
 int main(int argc, char const *argv[])
 {
     Vector_Matrix v1{{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6}};
-    print(Pooling(v1,"avg",2));
+    print(Pooling(v1,"min",2));
     return 0;
 }
--- assignment_1/convolution.cpp
+++ assignment_1/convolution.cpp
@@ -69,16 +69,44 @@ Vector_Matrix convolution(Vector_Matrix v1, Vector_Matrix v2){
     //print(v);
     return v;    
 }
-Vector_Matrix convolution_pad(Vector_Matrix v1, Vector_Matrix v2, int padding){
+Vector_Matrix convolution_pad(Vector_Matrix v2, int padding){
     Vector_Matrix pad(v2.size()+2*padding,vector<int>(v2[0].size()+2*padding,0));
     for(int q=0;q<v2.size();q++){
         for(int w=0;w<v2[0].size();w++){
             pad[q+padding][w+padding] = v2[q][w];
         }
     }
-    return convolution(v1,pad);
+    return pad;
 }
 
+// Vector_Matrix Convo_fast(Vector_Matrix v2, Vector_Matrix v1, string padding){
+//     //Part1: Making Matrix toeplitz
+//     //Part2: Matrix Multiplication
+
+//     //v1: Kernel
+//     int rows_v1 = v1.size();
+//     int rows_v2 = v2.size();
+//     int coloumns_v1 = v1[0].size();
+//     int coloumns_v2 = v2[0].size();
+//     int n = rows_v1;
+
+//     Vector_Matrix v((rows_v2-rows_v1+1),vector<int>((coloumns_v2-coloumns_v1+1),0)); //answer here
+//     Vector_Matrix v_temp(n,vector<int>(n,0)); //Extraction of smaller matrix from image matrix
+
+//     //Part1//
+//     //Adding padding
+//     string addpad = "addpadding";
+//     if(padding == addpad){
+//         int pad;
+//         pad = (rows_v1 - 1)/2;
+//         v2 = convolution_pad(v2, pad);
+//     }
+//     // make toeplitz matrix
+//     vector<int> lin_v1(rows_v1*coloumns_v1,0);
+//     vector<int> lin_v2(rows_v2*coloumns_v2,0);
+    
+//     Vector_Matrix toepMat()
+// }
 
 int main()
 {
@@ -86,7 +114,9 @@ int main()
   Vector_Matrix v2{{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6},{1,2,3,4,5,6}};
   Vector_Matrix v1{{1,2,3},{1,2,3},{1,2,3}};
   //vector<vector<int>> v3{{3,4,5},{3,4,5},{3,4,5}};
-    print(convolution_pad(v1,v2,1));
+  Vector_Matrix pad;
+  pad = convolution_pad(v2,1);
+    print(convolution(v1,pad));
   //cout<<sub_convo(v1,v2);
     
   return 0;
