d2ff9c5e26fc6f8bec15103e670421230b9d2c6c
Harshcs517 <cs5170409@cse.iitd.ac.in>
2019-01-13 16:15:59 +0530

    done some error handling

--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -229,6 +229,81 @@ float_Vector softmax(float_Vector a){
     return blu;
 }
 
+Vector_Matrix conv_in_matrix(char const *argv[],int n,int m){
+    stringstream convert1(argv[n]);
+    int size;
+    if (!(convert1 >> size)) // do the conversion
+        size = 0; // if conversion fails, set myint to a default value
+    
+    // main matrix v2
+    ifstream infile(argv[m]);
+    string line ="";
+    Vector_Matrix v2(size, vector<int>(size,0));
+    
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++)
+        {
+            getline(infile, line);
+            if(line != "")
+            {
+                stringstream num(line);
+                num >> n;
+                v2[j][i] = n;
+            }
+        }    
+    }
+    return v2;
+}
+int checker(char const *argv[], string forwhat)
+{
+    int n;
+    if(forwhat == "convolution")
+    {
+        if(sscanf(argv[3],"%d",&n) && sscanf(argv[5],"%d",&n) && argv[6]==NULL);
+        else 
+        {
+            cout<< "wrong" << endl;
+            cout<< "Expected - ";
+            cout<< argv[1] << " [matrix1.txt] [matrix1_size] [matrix2.txt] [matrix1_size]" << endl;
+            return 0;
+        }
+    }
+    else if(forwhat == "pooling")
+    {
+        if(sscanf(argv[2],"%d",&n) && sscanf(argv[4],"%d",&n) && sscanf(argv[5],"%d",&n) && argv[6]==NULL);
+        else
+        {
+            cout<< "wrong" << endl;
+            cout<< "Expected - ";
+            cout<< argv[1] << " [matrix1.txt] [matrix1_size] [stride]" << endl;
+            return 0;
+        }
+    }
+    else if(forwhat == "softmax")
+    {
+        if(sscanf(argv[3],"%d",&n) && argv[4]==NULL);
+        else
+        {
+            cout<< "wrong" << endl;
+            cout<< "Expected - ";
+            cout<< argv[1] << " [matrix1.txt] [matrix1_size] [stride]" << endl;
+            return 0;
+        }
+    }
+    else if(forwhat == "sigmoid")
+    {
+        if(sscanf(argv[3],"%d",&n) && argv[4]==NULL);
+        else
+        {
+            cout<< "wrong" << endl;
+            cout<< "Expected - ";
+            cout<< argv[1] << " [matrix1.txt] [matrix1_size] [stride]" << endl;
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main(int argc, char const *argv[])
 {
     string check ="";
@@ -238,239 +313,71 @@ int main(int argc, char const *argv[])
     // ./main.out convolution_withpadding_matrixmult padsize matrix1.txt matrix1_numrows matrix2.txt matrix2_numrows
     if(check == "convolution_withpadding_matrixmult"){
         //Read the file
-        stringstream convert(argv[2]);
-        int padsize;
-        if (!(convert >> padsize)) // do the conversion
-            padsize = 0; // if conversion fails, set myint to a default value
-        //main matrix size
-        stringstream convert1(argv[4]);
-        int size;
-        if (!(convert1 >> size)) // do the conversion
-            size = 0; // if conversion fails, set myint to a default value
-        
-        // main matrix v2
-        ifstream infile(argv[3]);
-        string line ="";
-        int n = 0;
-        Vector_Matrix v2(size, vector<int>(size,0));
-        
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-            {
-                getline(infile, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v2[j][i] = n;
-                }
-            }    
-        } 
-
-        // kernel row number
-        int size2 = 0;
-        stringstream convert2(argv[6]);
-        if (!(convert2 >> size2)) // do the conversion
-            size2 = 0; // if conversion fails, set myint to a default value
-        
-        //making of kernel v1
-        ifstream infile1(argv[5]);
-        line ="";
-        n = 0;
-        Vector_Matrix v1(size2, vector<int>(size2,0));
-        
-        for(int i = 0; i < size2; i++){
-            for(int j = 0; j < size2; j++)
-            {
-                getline(infile1, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v1[j][i] = n;
-                }
-            }    
+        if(!checker(argv,"convolution"))
+        {
+            return 0;
         }
-        int padding = (size2-1)/2;//optimal but not used now
-        v2 = convolution_pad(v2,padsize);
+
+        Vector_Matrix v2;
+        v2 = conv_in_matrix(argv,3,2);
+
+        
+        Vector_Matrix v1;
+        v1 = conv_in_matrix(argv,5,4);
+        int size2 = v1[0].size();
+        int padding = (size2-1)/2;
+        v2 = convolution_pad(v2,padding);
         print(convolution_matrixmult(v1,v2));
     }
     else if(check == "convolution_withoutpadding_matrixmult"){
-        stringstream convert(argv[2]);
-        int padsize;
-        if (!(convert >> padsize)) // do the conversion
-            padsize = 0; // if conversion fails, set myint to a default value
-        //main matrix size
-        stringstream convert1(argv[4]);
-        int size;
-        if (!(convert1 >> size)) // do the conversion
-            size = 0; // if conversion fails, set myint to a default value
-        
-        // main matrix v2
-        ifstream infile(argv[3]);
-        string line ="";
-        int n = 0;
-        Vector_Matrix v2(size, vector<int>(size,0));
-        
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-            {
-                getline(infile, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v2[j][i] = n;
-                }
-            }    
-        } 
-
-        // kernel row number
-        int size2 = 0;
-        stringstream convert2(argv[6]);
-        if (!(convert2 >> size2)) // do the conversion
-            size2 = 0; // if conversion fails, set myint to a default value
-        
-        //making of kernel v1
-        ifstream infile1(argv[5]);
-        line ="";
-        n = 0;
-        Vector_Matrix v1(size2, vector<int>(size2,0));
-        
-        for(int i = 0; i < size2; i++){
-            for(int j = 0; j < size2; j++)
-            {
-                getline(infile1, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v1[j][i] = n;
-                }
-            }    
+        //Read the file
+        if(!checker(argv,"convolution"))
+        {
+            return 0;
         }
+        Vector_Matrix v2;
+        v2 = conv_in_matrix(argv,3,2);
+
+        
+        Vector_Matrix v1;
+        v1 = conv_in_matrix(argv,5,4);
+        int size2 = v1[0].size();
         int padding = (size2-1)/2;//optimal but not used now
         // v2 = convolution_pad(v2,padsize);
         print(convolution_matrixmult(v1,v2));
 
     }
-    ////
-    ////
-    //this is of the form
     // ./main.out convolution_withpadding padsize matrix1.txt matrix1_numrows matrix2.txt matrix2_numrows
     else if(check == "convolution_withpadding"){
         //Read the file
-        stringstream convert(argv[2]);
-        int padsize;
-        if (!(convert >> padsize)) // do the conversion
-            padsize = 0; // if conversion fails, set myint to a default value
-        //main matrix size
-        stringstream convert1(argv[4]);
-        int size;
-        if (!(convert1 >> size)) // do the conversion
-            size = 0; // if conversion fails, set myint to a default value
-        
-        // main matrix v2
-        ifstream infile(argv[3]);
-        string line ="";
-        int n = 0;
-        Vector_Matrix v2(size, vector<int>(size,0));
-        
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-            {
-                getline(infile, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v2[j][i] = n;
-                }
-            }    
-        } 
-
-        // kernel row number
-        int size2 = 0;
-        stringstream convert2(argv[6]);
-        if (!(convert2 >> size2)) // do the conversion
-            size2 = 0; // if conversion fails, set myint to a default value
-        
-        //making of kernel v1
-        ifstream infile1(argv[5]);
-        line ="";
-        n = 0;
-        Vector_Matrix v1(size2, vector<int>(size2,0));
-        
-        for(int i = 0; i < size2; i++){
-            for(int j = 0; j < size2; j++)
-            {
-                getline(infile1, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v1[j][i] = n;
-                }
-            }    
+        if(!checker(argv,"convolution"))
+        {
+            return 0;
         }
+        Vector_Matrix v2;
+        v2 = conv_in_matrix(argv,3,2);
+
+        
+        Vector_Matrix v1;
+        v1 = conv_in_matrix(argv,5,4);
+        int size2 = v1[0].size();
         int padding = (size2-1)/2;//optimal but not used now
-        v2 = convolution_pad(v2,padsize);
+        v2 = convolution_pad(v2,padding);
         print(convolution(v1,v2));
     }
     else if(check == "convolution_withoutpadding"){
-        stringstream convert(argv[2]);
-        int padsize;
-        if (!(convert >> padsize)) // do the conversion
-            padsize = 0; // if conversion fails, set myint to a default value
-        //main matrix size
-        stringstream convert1(argv[4]);
-        int size;
-        if (!(convert1 >> size)) // do the conversion
-            size = 0; // if conversion fails, set myint to a default value
-        
-        // main matrix v2
-        ifstream infile(argv[3]);
-        string line ="";
-        int n = 0;
-        Vector_Matrix v2(size, vector<int>(size,0));
-        
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-            {
-                getline(infile, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v2[j][i] = n;
-                }
-            }    
-        } 
-
-        // kernel row number
-        int size2 = 0;
-        stringstream convert2(argv[6]);
-        if (!(convert2 >> size2)) // do the conversion
-            size2 = 0; // if conversion fails, set myint to a default value
-        
-        //making of kernel v1
-        ifstream infile1(argv[5]);
-        line ="";
-        n = 0;
-        Vector_Matrix v1(size2, vector<int>(size2,0));
-        
-        for(int i = 0; i < size2; i++){
-            for(int j = 0; j < size2; j++)
-            {
-                getline(infile1, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v1[j][i] = n;
-                }
-            }    
+        //Read the file
+        if(!checker(argv,"convolution"))
+        {
+            return 0;
         }
+        Vector_Matrix v2;
+        v2 = conv_in_matrix(argv,3,2);
+
+        
+        Vector_Matrix v1;
+        v1 = conv_in_matrix(argv,5,4);
+        int size2 = v1[0].size();
         int padding = (size2-1)/2;//optimal but not used now
         // v2 = convolution_pad(v2,padsize);
         print(convolution(v1,v2));
@@ -479,33 +386,15 @@ int main(int argc, char const *argv[])
     //// ./main.out pooling max matrix1.txt matrix1_numrows fil_size
     //// ./main.out pooling avg matrix1.txt matrix1_numrows fil_size
     else if(check == "pooling"){
+        if(!checker(argv,"pooling"))
+        {
+            return 0;
+        }
         string pool = "";
         stringstream convert(argv[2]);
         convert >> pool;
-        //main matrix size
-        stringstream convert1(argv[4]);
-        int size;
-        if (!(convert1 >> size)) // do the conversion
-            size = 0; // if conversion fails, set myint to a default value
-        
-        // main matrix v2
-        ifstream infile(argv[3]);
-        string line ="";
-        int n = 0;
-        Vector_Matrix v2(size, vector<int>(size,0));
-        
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++)
-            {
-                getline(infile, line);
-                if(line != "")
-                {
-                    stringstream num(line);
-                    num >> n;
-                    v2[j][i] = n;
-                }
-            }    
-        } 
+        Vector_Matrix v2;
+        v2 = conv_in_matrix(argv,4,3);
         int fil_size = 0;
         stringstream convert2(argv[5]);
         convert2 >> fil_size;
@@ -514,6 +403,10 @@ int main(int argc, char const *argv[])
     }
     // ./main.out softmax matrix1.txt matrix1_numrows
     else if(check == "softmax"){
+        if(!checker(argv,"softmax"))
+        {
+            return 0;
+        }
         stringstream convert1(argv[3]);
         int size;
         if (!(convert1 >> size)) // do the conversion
@@ -539,6 +432,10 @@ int main(int argc, char const *argv[])
     }
     // ./main.out sigmoid matrix1.txt matrix1_numrows
     else if(check == "sigmoid"){
+        if(!checker(argv,"sigmoid"))
+        {
+            return 0;
+        }
         stringstream convert1(argv[3]);
         int size;
         if (!(convert1 >> size)) // do the conversion
