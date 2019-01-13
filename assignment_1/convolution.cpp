8d6d557815d533e91ce6e4a96a22873fc3f015bf
Harshcs517 <cs5170409@cse.iitd.ac.in>
2019-01-13 13:07:41 +0530

    done matrixmult

--- assignment_1/convolution.cpp
+++ assignment_1/convolution.cpp
@@ -43,6 +43,74 @@ int sub_convo(Vector_Matrix v1, Vector_Matrix v2){
 //     }
 //     print(sol);
 // }
+Vector_Matrix convolution_matrixmult(Vector_Matrix v1, Vector_Matrix v2){
+    // v1: kernel
+    
+    int rows_v1 = v1.size();
+    int rows_v2 = v2.size();
+    int coloumns_v1 = v1[0].size();
+    int coloumns_v2 = v2[0].size();
+    int n = rows_v1;
+    
+    Vector_Matrix v((rows_v2-rows_v1+1),vector<int>((coloumns_v2-coloumns_v1+1),0)); //answer here
+    Vector_Matrix v_temp(n,vector<int>(n,0)); //Extraction of smaller matrix from image 
+    int columns_con_matmul= (coloumns_v2-coloumns_v1+1);
+    int rows_con_matmul = (rows_v2-rows_v1+1);
+    int col_row=columns_con_matmul*rows_con_matmul;
+
+    Vector_Matrix con_matmul(col_row, vector<int>(rows_v1*coloumns_v1,0));
+    int x_1=0,y_1=0;
+    for(int x=0;x<(rows_v2-rows_v1+1);x++){
+        for(int y=0;y<(coloumns_v2-coloumns_v1+1);y++){
+            for(int a=0;a<rows_v1;a++){
+                for(int b=0;b<coloumns_v1;b++){
+                    con_matmul[x_1][y_1] = v2[a+x][b+y];
+                    y_1++;
+                }
+            }
+            y_1 = 0;
+            x_1++;
+        }
+    }
+    int no_of_ele = rows_v1*coloumns_v1;
+    vector<int> v_templ(no_of_ele,0);
+    int count = 0;
+    for(int i = 0; i < rows_v1; i++)
+    {
+        for(int j = 0; j < coloumns_v1; j++)
+        {
+            v_templ[count] = v1[rows_v1-i-1][coloumns_v1-j-1];
+            count ++;
+        } 
+    }
+    
+    //Matrix multiplication
+    vector<int> sum(col_row,0);
+    if(no_of_ele == v_templ.size())
+    {
+        for(int i = 0; i < col_row; i++)
+        {
+            for(int j = 0; j < no_of_ele; j++)
+            {
+                sum[i]+=con_matmul[i][j]*v_templ[j];
+            }   
+        }
+    }
+    count = 0;
+    for(int i = 0; i < rows_con_matmul; i++)
+    {
+        for(int j = 0; j < columns_con_matmul; j++)
+        {
+            v[i][j] = sum[count];
+            count++;
+        }
+        
+    }
+    
+    //print(v);
+    return v;    
+}
+
 
 Vector_Matrix convolution(Vector_Matrix v1, Vector_Matrix v2){
     // v1: kernel
@@ -115,8 +183,12 @@ int main(int argc, char const *argv[])
   Vector_Matrix v1{{1,2,3},{1,2,3},{1,2,3}};
   //vector<vector<int>> v3{{3,4,5},{3,4,5},{3,4,5}};
   Vector_Matrix pad;
-  pad = convolution_pad(v2,0);
-    print(convolution(v1,pad));
+  pad = convolution_pad(v2,1);
+  print(convolution_matrixmult(v1,pad));
+
+  cout<<endl;
+
+  print(convolution(v1,pad));
   //cout<<sub_convo(v1,v2);
   return 0;
 }
--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -25,10 +25,10 @@ void print(Vector_Matrix v){
 // max pooling and average pooling are on the same program
 //      max pooling = 'max'   average pooling = 'avg'
 // assuming square matrix according to the problem statement
-Vector_Matrix Pooling(Vector_Matrix v1, string pool,int stride)
+Vector_Matrix Pooling(Vector_Matrix v1, string pool,int fil_size)
 {
     int size = v1.size();
-    int n = size/stride;
+    int n = size/fil_size;
     string max = "max";
     string avg = "avg";
     // This contains the answer
@@ -37,27 +37,27 @@ Vector_Matrix Pooling(Vector_Matrix v1, string pool,int stride)
     Vector_Matrix part(n,vector<int>(n,0));
 
     
-    for(int i = 0; i < size; i+=stride){
-        for(int j = 0; j < size; j+=stride){
+    for(int i = 0; i < size; i+=fil_size){
+        for(int j = 0; j < size; j+=fil_size){
             if (pool == max) {
                 int max = 0;
-                for(int l = 0; l < stride; l++){
-                    for(int m = 0; m < stride; m++){
+                for(int l = 0; l < fil_size; l++){
+                    for(int m = 0; m < fil_size; m++){
                         if(max < v1[i+l][j+m]){
                             max = v1[i+l][j+m];
                         }
                     }  
                 }
-                ans[i/stride][j/stride] = max;
+                ans[i/fil_size][j/fil_size] = max;
             }  
             else if (pool == avg) {
                 int avg = 0;
-                for(int l = 0; l < stride; l++){
-                    for(int m = 0; m < stride; m++){
+                for(int l = 0; l < fil_size; l++){
+                    for(int m = 0; m < fil_size; m++){
                         avg+=v1[i+l][j+m];
                     }  
                 }
-                ans[i/stride][j/stride] = avg/(stride*stride);
+                ans[i/fil_size][j/fil_size] = avg/(fil_size*fil_size);
             }  
             else 
             {
@@ -127,6 +127,75 @@ Vector_Matrix convolution_pad(Vector_Matrix v2, int padding){
     return pad;
 }
 
+Vector_Matrix convolution_matrixmult(Vector_Matrix v1, Vector_Matrix v2){
+    // v1: kernel
+    
+    int rows_v1 = v1.size();
+    int rows_v2 = v2.size();
+    int coloumns_v1 = v1[0].size();
+    int coloumns_v2 = v2[0].size();
+    int n = rows_v1;
+    
+    Vector_Matrix v((rows_v2-rows_v1+1),vector<int>((coloumns_v2-coloumns_v1+1),0)); //answer here
+    Vector_Matrix v_temp(n,vector<int>(n,0)); //Extraction of smaller matrix from image 
+    int columns_con_matmul= (coloumns_v2-coloumns_v1+1);
+    int rows_con_matmul = (rows_v2-rows_v1+1);
+    int col_row=columns_con_matmul*rows_con_matmul;
+
+    Vector_Matrix con_matmul(col_row, vector<int>(rows_v1*coloumns_v1,0));
+    int x_1=0,y_1=0;
+    for(int x=0;x<(rows_v2-rows_v1+1);x++){
+        for(int y=0;y<(coloumns_v2-coloumns_v1+1);y++){
+            for(int a=0;a<rows_v1;a++){
+                for(int b=0;b<coloumns_v1;b++){
+                    con_matmul[x_1][y_1] = v2[a+x][b+y];
+                    y_1++;
+                }
+            }
+            y_1 = 0;
+            x_1++;
+        }
+    }
+    int no_of_ele = rows_v1*coloumns_v1;
+    vector<int> v_templ(no_of_ele,0);
+    int count = 0;
+    for(int i = 0; i < rows_v1; i++)
+    {
+        for(int j = 0; j < coloumns_v1; j++)
+        {
+            v_templ[count] = v1[rows_v1-i-1][coloumns_v1-j-1];
+            count ++;
+        } 
+    }
+    
+    //Matrix multiplication
+    vector<int> sum(col_row,0);
+    if(no_of_ele == v_templ.size())
+    {
+        for(int i = 0; i < col_row; i++)
+        {
+            for(int j = 0; j < no_of_ele; j++)
+            {
+                sum[i]+=con_matmul[i][j]*v_templ[j];
+            }   
+        }
+    }
+    count = 0;
+    for(int i = 0; i < rows_con_matmul; i++)
+    {
+        for(int j = 0; j < columns_con_matmul; j++)
+        {
+            v[i][j] = sum[count];
+            count++;
+        }
+        
+    }
+    
+    //print(v);
+    return v;    
+}
+
+
 /* ---------- SOftmax ----------- */
 void print(float_Vector v){
     for(int i=0;i<v.size();i++){
@@ -162,12 +231,134 @@ float_Vector softmax(float_Vector a){
 
 int main(int argc, char const *argv[])
 {
-    //this is of the form
-    // ./main.out convolution_withpadding_matrixmult padsize matrix1.txt matrix1_numrows matrix2.txt matrix2_numrows
     string check ="";
     stringstream strtake(argv[1]);
     strtake >> check;
+    //this is of the form
+    // ./main.out convolution_withpadding_matrixmult padsize matrix1.txt matrix1_numrows matrix2.txt matrix2_numrows
     if(check == "convolution_withpadding_matrixmult"){
+        //Read the file
+        stringstream convert(argv[2]);
+        int padsize;
+        if (!(convert >> padsize)) // do the conversion
+            padsize = 0; // if conversion fails, set myint to a default value
+        //main matrix size
+        stringstream convert1(argv[4]);
+        int size;
+        if (!(convert1 >> size)) // do the conversion
+            size = 0; // if conversion fails, set myint to a default value
+        
+        // main matrix v2
+        ifstream infile(argv[3]);
+        string line ="";
+        int n = 0;
+        Vector_Matrix v2(size, vector<int>(size,0));
+        
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++)
+            {
+                getline(infile, line);
+                if(line != "")
+                {
+                    stringstream num(line);
+                    num >> n;
+                    v2[j][i] = n;
+                }
+            }    
+        } 
+
+        // kernel row number
+        int size2 = 0;
+        stringstream convert2(argv[6]);
+        if (!(convert2 >> size2)) // do the conversion
+            size2 = 0; // if conversion fails, set myint to a default value
+        
+        //making of kernel v1
+        ifstream infile1(argv[5]);
+        line ="";
+        n = 0;
+        Vector_Matrix v1(size2, vector<int>(size2,0));
+        
+        for(int i = 0; i < size2; i++){
+            for(int j = 0; j < size2; j++)
+            {
+                getline(infile1, line);
+                if(line != "")
+                {
+                    stringstream num(line);
+                    num >> n;
+                    v1[j][i] = n;
+                }
+            }    
+        }
+        int padding = (size2-1)/2;//optimal but not used now
+        v2 = convolution_pad(v2,padsize);
+        print(convolution_matrixmult(v1,v2));
+    }
+    else if(check == "convolution_withoutpadding_matrixmult"){
+        stringstream convert(argv[2]);
+        int padsize;
+        if (!(convert >> padsize)) // do the conversion
+            padsize = 0; // if conversion fails, set myint to a default value
+        //main matrix size
+        stringstream convert1(argv[4]);
+        int size;
+        if (!(convert1 >> size)) // do the conversion
+            size = 0; // if conversion fails, set myint to a default value
+        
+        // main matrix v2
+        ifstream infile(argv[3]);
+        string line ="";
+        int n = 0;
+        Vector_Matrix v2(size, vector<int>(size,0));
+        
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++)
+            {
+                getline(infile, line);
+                if(line != "")
+                {
+                    stringstream num(line);
+                    num >> n;
+                    v2[j][i] = n;
+                }
+            }    
+        } 
+
+        // kernel row number
+        int size2 = 0;
+        stringstream convert2(argv[6]);
+        if (!(convert2 >> size2)) // do the conversion
+            size2 = 0; // if conversion fails, set myint to a default value
+        
+        //making of kernel v1
+        ifstream infile1(argv[5]);
+        line ="";
+        n = 0;
+        Vector_Matrix v1(size2, vector<int>(size2,0));
+        
+        for(int i = 0; i < size2; i++){
+            for(int j = 0; j < size2; j++)
+            {
+                getline(infile1, line);
+                if(line != "")
+                {
+                    stringstream num(line);
+                    num >> n;
+                    v1[j][i] = n;
+                }
+            }    
+        }
+        int padding = (size2-1)/2;//optimal but not used now
+        // v2 = convolution_pad(v2,padsize);
+        print(convolution_matrixmult(v1,v2));
+
+    }
+    ////
+    ////
+    //this is of the form
+    // ./main.out convolution_withpadding padsize matrix1.txt matrix1_numrows matrix2.txt matrix2_numrows
+    else if(check == "convolution_withpadding"){
         //Read the file
         stringstream convert(argv[2]);
         int padsize;
@@ -226,7 +417,7 @@ int main(int argc, char const *argv[])
         v2 = convolution_pad(v2,padsize);
         print(convolution(v1,v2));
     }
-    else if(check == "convolution_withoutpadding_matrixmult"){
+    else if(check == "convolution_withoutpadding"){
         stringstream convert(argv[2]);
         int padsize;
         if (!(convert >> padsize)) // do the conversion
@@ -285,8 +476,8 @@ int main(int argc, char const *argv[])
         print(convolution(v1,v2));
 
     }
-    //// ./main.out pooling max matrix1.txt matrix1_numrows stride
-    //// ./main.out pooling avg matrix1.txt matrix1_numrows stride
+    //// ./main.out pooling max matrix1.txt matrix1_numrows fil_size
+    //// ./main.out pooling avg matrix1.txt matrix1_numrows fil_size
     else if(check == "pooling"){
         string pool = "";
         stringstream convert(argv[2]);
@@ -315,10 +506,10 @@ int main(int argc, char const *argv[])
                 }
             }    
         } 
-        int stride = 0;
+        int fil_size = 0;
         stringstream convert2(argv[5]);
-        convert2 >> stride;
-        print(Pooling(v2,pool,stride));
+        convert2 >> fil_size;
+        print(Pooling(v2,pool,fil_size));
         
     }
     // ./main.out softmax matrix1.txt matrix1_numrows
