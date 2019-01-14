05464a6ae16135b8c515f35e865a063599ae8217
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-01-14 16:57:41 +0530

    Update sigmoid_softmax.cpp

--- assignment_1/sigmoid_softmax.cpp
+++ assignment_1/sigmoid_softmax.cpp
@@ -3,42 +3,11 @@
 #include <vector>
 #include <random>
 #include <cmath>
-#define float_Vector vector<float>
-#define forLoop for(int i=0;i<v.size();i++)
+#include "sigmoid_softmax.h"
+#include "printHead.h"
+
 using namespace std;
 
-
-void print(float_Vector v){
-    for(int i=0;i<v.size();i++){
-      cout<<v[i]<<" ";
-  }
-}
-void print_sum(float_Vector v){
-    float ans = 0.0;
-    for(int i=0;i<v.size();i++){
-        ans += v[i];
-    }
-    cout<<"***"<<ans<<"***";
-}
-float_Vector sigmoid(float_Vector a){
-    float_Vector ans(a.size(),0.0);
-    for(int i=0;i<a.size();i++){
-        ans[i] = 1/(1+exp(-1*a[i]));
-    }
-    return ans;
-}
-float_Vector softmax(float_Vector a){
-    float_Vector blu(a.size(),0.0);
-    float sum = 0.0;
-    for(int i=0;i<a.size();i++){
-        sum += exp(a[i]);
-    }
-    //cout<<"AAA "<<sum<<" ";
-    for(int j=0;j<a.size();j++){
-        blu[j] = (exp(a[j]))/sum;
-    }
-    return blu;
-}
 int main()
 {
   mt19937 mt(1729); //comes from #include <random>: New method for random number generation with much more distributed numbers
@@ -49,8 +18,6 @@ int main()
       v[i] = (float)(dist(mt)/3);
   }
   print(softmax(v));
-  //cout<<endl;
-  //print_sum(softmax(v));
     
   return 0;
 }
