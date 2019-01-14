06b8601d41bc084354e821088587bde59dbf00aa
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-01-14 16:55:35 +0530

    Update Pooling.cpp

--- assignment_1/Pooling.cpp
+++ assignment_1/Pooling.cpp
@@ -3,64 +3,63 @@
 #include <vector>
 #define Vector_Matrix vector<vector<int>>
 using namespace std;
+#include "Pooling.h"
+#include "printHead.h"
 
 // max pooling and average pooling are on the same program
 //      max pooling = 'max'   average pooling = 'avg'
 // assuming square matrix according to the problem statement
-void print(Vector_Matrix v){
-    for(int i=0;i<v.size();i++){
-      for(int j=0;j<v[0].size();j++){
-          cout<<v[i][j]<<" ";
-          
-      }
-      cout<<endl;
-  }
-}
+
+// Vector_Matrix advancePadding(Vector_Matrix v, int stride){
+//     int rows = v.size();
+//     int coloumn
+// }
+
 //max pooling function
-Vector_Matrix Pooling(Vector_Matrix v1, string pool,int stride)
-{
-    int size = v1.size();
-    int n = size/stride;
-    string max = "max";
-    string avg = "avg";
-    // This contains the answer
-    Vector_Matrix ans(n,vector<int>((n),0));
-    // This contains Part to check
-    Vector_Matrix part(n,vector<int>(n,0));
+// Vector_Matrix Pooling(Vector_Matrix v1, string pool,int stride)
+// {
+//     int size = v1.size();
+//     int n = size/stride;
+//     string max = "max";
+//     string avg = "avg";
+//     // This contains the answer
+//     Vector_Matrix ans(n,vector<int>((n),0));
+//     // This contains Part to check
+//     Vector_Matrix part(n,vector<int>(n,0));
 
     
-    for(int i = 0; i < size; i+=stride){
-        for(int j = 0; j < size; j+=stride){
-            if (pool == max) {
-                int max = 0;
-                for(int l = 0; l < stride; l++){
-                    for(int m = 0; m < stride; m++){
-                        if(max < v1[i+l][j+m]){
-                            max = v1[i+l][j+m];
-                        }
-                    }  
-                }
-                ans[i/stride][j/stride] = max;
-            }  
-            else if (pool == avg) {
-                int avg = 0;
-                for(int l = 0; l < stride; l++){
-                    for(int m = 0; m < stride; m++){
-                        avg+=v1[i+l][j+m];
-                    }  
-                }
-                ans[i/stride][j/stride] = avg/(stride*stride);
-            }  
-            else 
-            {
-                cout << "choose 'max' or 'avg' pool" << endl; 
-                return ans;
-            }   
-        }
+//     for(int i = 0; i < size; i+=stride){
+//         for(int j = 0; j < size; j+=stride){
+//             if (pool == max) {
+//                 int max = 0;
+//                 for(int l = 0; l < stride; l++){
+//                     for(int m = 0; m < stride; m++){
+//                         if(max < v1[i+l][j+m]){
+//                             max = v1[i+l][j+m];
+//                         }
+//                     }  
+//                 }
+//                 ans[i/stride][j/stride] = max;
+//             }  
+//             else if (pool == avg) {
+//                 int avg = 0;
+//                 for(int l = 0; l < stride; l++){
+//                     for(int m = 0; m < stride; m++){
+//                         avg+=v1[i+l][j+m];
+//                     }  
+//                 }
+//                 ans[i/stride][j/stride] = avg/(stride*stride);
+//             }  
+//             else 
+//             {
+//                 cout << "choose 'max' or 'avg' pool" << endl; 
+//                 return ans;
+//             }   
+//         }
         
-    }
-    return ans;
-}
+//     }
+//     return ans;
+// }
 
 int main(int argc, char const *argv[])
 {
