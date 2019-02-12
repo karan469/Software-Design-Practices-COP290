0406f0d0dba88106fa07c6c00379f11cc2ac6a17
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-02-13 00:15:35 +0530

    Done all Layers including FC excluding bias

--- final/lenet.cpp
+++ final/lenet.cpp
@@ -4,17 +4,7 @@
 #define Vector_Matrix_Float vector<vector<float>>
 #define Vector_Tetris_Float vector<vector<vector<float>>>
 
-class kernels{
-private:
-	Vector_Matrix_Float filters[6];
 
-	void updateKernel(int i, Vector_Matrix_Float v){
-		filters[i] = v;
-	}
-	Vector_Matrix_Float returnIthkernel(int i){
-		return filters[i];
-	}
-};
 
 // class poolingKernel{
 // private:
@@ -37,8 +27,6 @@ private:
 // 	return v;
 // }
 
-
-
 Vector_Matrix_Float giveOneSheet(Vector_Tetris_Float v, int rows, int coloumns, int h){
 	Vector_Matrix_Float v1(rows,vector<float>(coloumns,0.0));
 	for(int i=0;i<rows;i++){
@@ -58,6 +46,7 @@ Vector_Tetris_Float Conv_1(Vector_Matrix_Float image){
 		for(int j=0;j<rows;j++){
 			for(int k=0;k<coloumns;k++){
 				v[j][k][i] = convolution_matrixmult_mkl(image,k.filters[i],"intelmkl")[j][k];
+				//DO BIAS
 			}
 		}
 	}
@@ -87,7 +76,8 @@ Vector_Tetris_Float Conv_2(Vector_Tetris_Float image3d, Vector_Tetris_Float filt
 	//image3d: 12 X 12 X 20
 	//filter: 5 X 5 X 20
 	Vector_Tetris_Float v(8,vector<vector<float>(8,vector<float>(50,0.0)));
-	v = convolution3d(image3d,filter,12,5);//convolution3d() to be made
+	v = convolution3d(image3d,12,filter,5,20);//convolution3d() to be made
+	//DO BIAS
 	return v;
 }
 
@@ -110,11 +100,41 @@ Vector_Tetris_Float Pool_2(Vector_Tetris_Float image3d){
 }
 
 // Vector_Tetris_Float volumeConvolution(Vector_Tetris_Float image3d, int n1, Vector_Tetris_Float kernel, int n2){
-	
+
 // }
 
-Vector_Tetris_Float FC_1(Vector_Tetris_Float image3d, Vector_Tetris_Float filter){
-	
+Vector_Tetris_Float FC_1(Vector_Tetris_Float image3d, vector<vector<vector<vector<float> > > > filter){
+	Vector_Tetris_Float v(1,vector<vector<float>>(1,vector<float>(500,0.0)));
+	Vector_Tetris_Float filterCube(4,vector<vector<float>>(4,vector<float>(50,0.0)));
+	for(int i=0;i<500;i++){
+		
+		for(int j=0;j<4;j++){
+			for(int k=0;k<4;k++){
+				for(int t=0;t<50;t++){
+					filterCube[j][k][t] = filter[j][k][t][i];
+				}
+			}
+		}
+		v[0][0][i] = convolution3d(image3d, 4, filterCube, 4, 50);
+		//DO BIAS
+	}
+	return v;
+}
+
+Vector_Matrix_Float FC_half(Vector_Tetris_Float outFromFC1){
+	Vector_Matrix_Float inForFC2(1,vector<float>(500,0.0));
+	for(int h=0;h<500;h++){
+		inForFC2[0][h] = outFromFC1[0][0][h];
+	}
+	return inForFC2;
+}
+
+Vector_Matrix_Float FC_2(Vector_Matrix_Float inForFC2, Vector_Matrix_Float filters){
+	//this gives 1 X 10
+	Vector_Matrix_Float v(1,vector<float>(10,0.0));
+	v = convolution_matrixmult_mkl(inForFC2, filters);
+	//DO BIAS
+	return v;
 }
 
 int main(int argc, char const *argv[])
