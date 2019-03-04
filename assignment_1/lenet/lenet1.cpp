25e1e26d8e83dc7aed211243214a136b5242a10d
Karan Tanwar <33158202+CRekkaran@users.noreply.github.com>
2019-03-04 15:38:05 +0530

    Update lenet1.cpp

--- assignment_1/lenet/lenet1.cpp
+++ assignment_1/lenet/lenet1.cpp
@@ -2,12 +2,12 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include "../Pooling.h"
-#include "../convolution_mkl.h"
-#include "../sigmoid_softmax.h"
-#include "../convolution.h"
-#include "../printHead.h"
-#include "../convolution_pthread.h"
+#include "../imgProcess/Pooling.h"
+#include "../imgProcess/convolution_mkl.h"
+#include "../imgProcess/sigmoid_softmax.h"
+#include "../imgProcess/convolution.h"
+#include "../imgProcess/printHead.h"
+#include "../imgProcess/relu_tanh.h"
 #include "takeFilters.h"
 #include "/home/karan/intel/compilers_and_libraries_2019.1.144/linux/mkl/include/mkl.h"
 
@@ -114,49 +114,49 @@ Vector_Tetris_Float Pool_1(Vector_Tetris_Float image3d){
 }
 
 // Second version of Pool_1 much cleaner but not using now coz its incomplete, bitch
-Vector_Tetris_Float Pool_1_1(Vector_Tetris_Float image3d){
-	struct filterAndBias data;
-	data = filterBias("conv2.txt",5,20,20);
-	Vector_Tetris_Float v(20,vector<vector<float>>(12,vector<float>(12,0.0)));
-	for(int i=0;i<20;i++){
-		v[i] = Pooling(image3d[i],"max",2);
-	}
-	return v;
-}
+// Vector_Tetris_Float Pool_1_1(Vector_Tetris_Float image3d){
+// 	struct filterAndBias data;
+// 	data = filterBias("conv2.txt",5,20,20);
+// 	Vector_Tetris_Float v(20,vector<vector<float>>(12,vector<float>(12,0.0)));
+// 	for(int i=0;i<20;i++){
+// 		v[i] = Pooling(image3d[i],"max",2);
+// 	}
+// 	return v;
+// }
 
 // This funciton reminds me of ocaml, sounds good but is shit :/
-Vector_Tetris_Float convolution3dH(struct filterAndBias4d data,Vector_Tetris_Float image3d,int a,int b,int c,int d)
-{
-	//a: output channel depth
-	//b: input channel depth
-	//c, d: output sheet dimension
-	Vector_Tetris_Float v(c,vector<vector<float>>(d,vector<float>(a,0.0)));
-	for(int l = 0; l < a; l++)
-	{
-		for(int k = 0; k < b; k++)
-		{
-			Vector_Matrix_Float tempmatrix = convolution_matrixmult_mkl(data.filter[l][k],image3d[k],"intelmkl");
-			for(int i = 0; i < c; i++)
-			{	
-				for(int j = 0; j < d; j++)
-				{
-					v[l][i][j] += tempmatrix[i][j];
-				}	
-			}
-		}
-	}
-	for(int l = 0; l < a; l++)
-	{
-		for(int i = 0; i < c; i++)
-		{
-			for(int j = 0; j < d; j++)
-			{
-				v[l][i][j] += data.bias[l][0];
-			}	
-		}
-	}
-	return v;
-}
+// Vector_Tetris_Float convolution3dH(struct filterAndBias4d data,Vector_Tetris_Float image3d,int a,int b,int c,int d)
+// {
+// 	//a: output channel depth
+// 	//b: input channel depth
+// 	//c, d: output sheet dimension
+// 	Vector_Tetris_Float v(c,vector<vector<float>>(d,vector<float>(a,0.0)));
+// 	for(int l = 0; l < a; l++)
+// 	{
+// 		for(int k = 0; k < b; k++)
+// 		{
+// 			Vector_Matrix_Float tempmatrix = convolution_matrixmult_mkl(data.filter[l][k],image3d[k],"intelmkl");
+// 			for(int i = 0; i < c; i++)
+// 			{	
+// 				for(int j = 0; j < d; j++)
+// 				{
+// 					v[l][i][j] += tempmatrix[i][j];
+// 				}	
+// 			}
+// 		}
+// 	}
+// 	for(int l = 0; l < a; l++)
+// 	{
+// 		for(int i = 0; i < c; i++)
+// 		{
+// 			for(int j = 0; j < d; j++)
+// 			{
+// 				v[l][i][j] += data.bias[l][0];
+// 			}	
+// 		}
+// 	}
+// 	return v;
+// }
 
 Vector_Tetris_Float Conv_2(Vector_Tetris_Float image3d){
 	//image3d: 12 X 12 X 20
@@ -173,28 +173,10 @@ Vector_Tetris_Float Conv_2(Vector_Tetris_Float image3d){
 	for(int i=0;i<50;i++){
 		//for(int j=0;j<20;j++){
 			//v[i] = addMatrices(v[i], convolution_matrixmult_pthread(data.filter[i][j], image3d[j])) ;
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][0]), image3d[0], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][1]), image3d[1], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][2]), image3d[2], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][3]), image3d[3], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][4]), image3d[4], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][5]), image3d[5], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][6]), image3d[6], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][7]), image3d[7], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][8]), image3d[8], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][9]), image3d[9], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][10]), image3d[10], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][11]), image3d[11], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][12]), image3d[12], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][13]), image3d[13], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][14]), image3d[14], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][15]), image3d[15], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][16]), image3d[16], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][17]), image3d[17], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][18]), image3d[18], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][19]), image3d[19], "intelmkl"));
-
-		//}
+			for(int j = 0; j < 20; j++)
+			{
+				v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[i][j]), image3d[j], "intelmkl"));
+			}
 	}
 	//Added BIAS//
 	for(int q=0;q<50;q++){
@@ -231,8 +213,7 @@ Vector_Tetris_Float FC_1(Vector_Tetris_Float image3d){
 	//image3d: 4*4*50
 	struct filterAndBias4d data;
 	data = filterBias4d("fc1.txt",500,4,500,50);
-	Vector_Tetris_Float v(500,vector<vector<float>>(1,vector<float>(1,0.0)));
-
+	Vector_Tetris_Float v(500,vector<vector<float>>(1,vector<float>(1,0.0f)));
 
 	//3D CONVOLUTION//
 	//print(data.filter[1][10]);
@@ -240,63 +221,25 @@ Vector_Tetris_Float FC_1(Vector_Tetris_Float image3d){
 		//cout<<"asdasd"<<endl;
 		//for(int j=0;j<20;j++){
 			//v[i] = addMatrices(v[i], convolution_matrixmult_pthread(data.filter[i][j], image3d[j])) ;
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][0]), image3d[0], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][1]), image3d[1], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][2]), image3d[2], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][3]), image3d[3], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][4]), image3d[4], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][5]), image3d[5], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][6]), image3d[6], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][7]), image3d[7], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][8]), image3d[8], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][9]), image3d[9], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][10]), image3d[10], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][11]), image3d[11], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][12]), image3d[12], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][13]), image3d[13], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][14]), image3d[14], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][15]), image3d[15], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][16]), image3d[16], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][17]), image3d[17], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][18]), image3d[18], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][19]), image3d[19], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][20]), image3d[20], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][21]), image3d[21], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][22]), image3d[22], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][23]), image3d[23], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][24]), image3d[24], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][25]), image3d[25], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][26]), image3d[26], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][27]), image3d[27], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][28]), image3d[28], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][29]), image3d[29], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][30]), image3d[30], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][31]), image3d[31], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][32]), image3d[32], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][33]), image3d[33], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][34]), image3d[34], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][35]), image3d[35], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][36]), image3d[36], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][37]), image3d[37], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][38]), image3d[38], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][39]), image3d[39], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][40]), image3d[40], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][41]), image3d[41], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][42]), image3d[42], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][43]), image3d[43], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][44]), image3d[44], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][45]), image3d[45], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][46]), image3d[46], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][47]), image3d[47], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][48]), image3d[48], "intelmkl"));
-			v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0][49]), image3d[49], "intelmkl"));
-		//}
+			for(int j = 0; j < 50; j++)
+			{
+				v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[i][j]), image3d[j], "intelmkl"));
+			}
+			
+
 	}
 	//Added Biasing//
 	for(int q=0;q<500;q++){
 		for(int a=0;a<1;a++){
 			for(int b=0;b<1;b++){
 				v[q][a][b] += data.bias[q][0];
+			}
+		}
+	}
+	for(int q=0;q<500;q++){
+		for(int a=0;a<1;a++){
+			for(int b=0;b<1;b++){
+				v[q][a][b] = makeRelu(v[q][a][b]);
 			}
 		}
 	}
@@ -352,35 +295,54 @@ vector<float> mainController(string filename){
 	return softmax(FC_Final(FC_2(FC_1(Pool_2(Conv_2(Pool_1(Conv_1(image))))))));
 }
 
+void maxExpectations(vector<float> v){
+	vector<float> a(5,0.0);
+	vector<float> b(5,0.0);
+	for(int h=0;h<5;h++){
+		a[h] = v[0];
+		for(int i =0;i<v.size();i++){
+			if(v[i]>a[h]){
+				a[h]=v[i];
+				b[h]=i;
+			}
+		}
+		v[(b[h])]=0;
+	}
+	cout<<"---------------------------------------------------------"<<endl;
+	cout<<"Prediction on the numbers with highest 5 probabilities: ";
+	for(int k=0;k<5;k++){
+		cout<<endl<<b[k]<<": "<<a[k]*100<<" %";
+	}
+	cout<<endl<<"---------------------------------------------------------";
+	cout<<endl;
+
+}
+
 int main()
 {
-	cout<<"\n\n\n";
-	//print(FC_Final(FC_1(Pool_2(Conv_2(Pool_1(Conv_1(imageFromText("1_new.txt")))))))); //testing result from Conv_1
+	cout<<"\n";
+	// string filename;
+	// cout<<"Enter img filename: ";
+	// cin>>filename;
+	// cout<<endl;
+	// maxExpectations(mainController(filename));
+	maxExpectations(mainController("image.txt"));
+	//print(mainController("image.txt"));
+	
+	//print(FC_2(FC_1(Pool_2(Conv_2(Pool_1(Conv_1(imageFromText("1_new.txt")))))))[1]);
+	//Vector_Tetris_Float fuck = (((Conv_2(Pool_1(Conv_1(imageFromText("1_new.txt")))))));
+	//print(((fuck))[2]); //testing result from Conv_1
 	//cout<<FC_1(Pool_2(Conv_2(Pool_1(Conv_1(imageFromText("1_new.txt")))))).size();
 	
-	
-	//print(mainController("1_new.txt"));
+	//print(mainController("2_new.txt"));
 	//print(Conv_1(imageFromText("1_new.txt"))[0]);
 
 	//print(imageFromText("1_new.txt"));//image matrix from txt
-	//print(mainController("2_new.txt"));//final output
-	
-	// Vector_Matrix_Float wii(3,vector<float>(3,0.0));
-	// for(int i=0;i<3;i++){
-	// 	for(int j=0;j<3;j++){
-	// 		wii[i][j]=i+j+1+1;
-	// 	}
-	// }
-	// print(wii);
-	// print(convolution_matrixmult_mkl(rotateF(wii),(wii),"intelmkl") );
+	//print(mainController("1_new.txt"));//final output
 	
 	//Checking takeFilter.h
 	//filterBias("conv1.txt",)
 
-	cout<<endl;
-	
-	// for(int q=0;q<50;q++)
-	// 	cout<<"v[i] = addMatrices (v[i],convolution_matrixmult_mkl(rotateF(data.filter[0]["<<q<<"]), image3d["<<q<<"], \"intelmkl\"));"<<endl;
-	
+	cout<<endl;	
 	return 0;
 }
